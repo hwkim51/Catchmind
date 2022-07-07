c2a2ffffc4e1260c640b5a9dcab7e16042819fe9
www78 <subin708@naver.com>
2022-07-08 06:42:13 +0900

    member 관련 소스코드 수정(수빈)
    member 관련 소스코드 수정(수빈)

--- Catchmind/src/main/java/com/e1i4/catchmind/member/controller/MemberController.java
+++ Catchmind/src/main/java/com/e1i4/catchmind/member/controller/MemberController.java
@@ -1,5 +1,11 @@
 package com.e1i4.catchmind.member.controller;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.e1i4.catchmind.member.model.service.MemberService;
 import com.e1i4.catchmind.member.model.vo.Member;
@@ -46,11 +53,6 @@ public class MemberController {
 		return ""; 
 	}
 	
-	@RequestMapping(value="enrollForm.me")
-	public String enrollForm() {
-		return "member/memberEnrollForm";
-	}
-	
 	// 아이디 중복체크(select)
 	@ResponseBody
 	@RequestMapping(value="idCheck.me", produces="text/html; charset=UTF-8")
@@ -59,6 +61,14 @@ public class MemberController {
 		return (count>0)?"NNNNN" : "NNNNY"; 
 	}
 	
+	// 닉네임 중복체크(select)
+	@ResponseBody
+	@RequestMapping(value="nicknameCheck.me", produces="text/html; charset=UTF-8")
+	public String nicknameCheck(String nickname) {
+		int count = memberService.nicknameCheck(nickname);
+		return (count>0)? "NNNNN" : "NNNNY"; 
+	}
+		
 	// 아이디 찾기 페이지로 이동
 	@RequestMapping(value="findIdPage.me")
 	public String findIdPage() {	
@@ -66,14 +76,15 @@ public class MemberController {
 	}
 	
 	// 아이디 찾기 
-	@RequestMapping(value="findId.me")
+	@RequestMapping(value="result.me")
 	public String findId(
 							Member m,
-							Model model
+							Model model,
+							HttpSession session
 			) {	
 		Member findUser = memberService.findId(m);
+		session.setAttribute("resultMsg", "입력한 정보로 찾은 회원 조회 결과 입니다.");
 		model.addAttribute("findUser",findUser);
-		
 		return "member/findResult"; 	
 	}
 	
@@ -95,6 +106,9 @@ public class MemberController {
 		return "member/changePwd"; 	
 	}
 	
+	// 비밀번호 찾기 - ajax로 이메일 인증번호 보내기
+	
+	
 //	// 비밀번호 찾기 - 변경 ( 실패 ing임 .. ^^ (수빈))
 //	@ResponseBody
 //	@RequestMapping(value="changePwd.me", produces="text/html; charset=UTF-8")
@@ -110,21 +124,54 @@ public class MemberController {
 //		return (result>0)?"NNNNN" : "NNNNY"; 
 //	}
 	
+	// 회원가입 페이지로 이동
+		@RequestMapping(value="enrollForm.me")
+		public String enrollForm() {
+			return "member/memberEnrollForm";
+		}
+		
 	// 회원가입(insert)
-//	@RequestMapping(value="insert.me")
-//	public String insertMember(Member m) {
-//		
-//		
-//		return "member/login";
-//	}
+	@RequestMapping(value="insert.me")
+	public String insertMember(
+								Member m,
+								MultipartFile upfilePic,
+								Model model,
+								HttpSession session
+								) {
+		
+		if(!upfilePic.getOriginalFilename().equals("")) {
+			String changeName = saveFile(upfilePic, session);
+			m.setPic("resources/picUploadFiles/" + changeName);
+			
+		}
+		System.out.println(m);
+		int result = memberService.insertMember(m);
+		
+		if(result > 0 ) {
+			session.setAttribute("resultMsg", "성공적으로 회원가입이 완료되었습니다.");
+			model.addAttribute("findUser",m);
+				return "member/findResult";
+		}else {
+			model.addAttribute("errorMsg","회원가입 실패");
+			return "errorPage";
+		}
+	}
 	
-	// 닉네임 중복체크(select)
-	@ResponseBody
-	@RequestMapping(value="nicknameCheck.me", produces="text/html; charset=UTF-8")
-	public String nicknameCheck(String nickname) {
-		int count = memberService.nicknameCheck(nickname);
-		System.out.println(count);
-		return (count>0)? "NNNNN" : "NNNNY"; 
+	// 회원가입 시 프로필 사진 저장 메소드
+	public String saveFile(MultipartFile upfilePic, HttpSession session) {
+		
+		String originName = upfilePic.getOriginalFilename();
+		String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+		int ranNum = (int)(Math.random() * 90000 + 10000);
+		String ext = originName.substring(originName.lastIndexOf("."));
+		String changeName = currentTime + ranNum + ext;
+		String savePath = session.getServletContext().getRealPath("/resources/picUploadFiles/");
+		try {
+			upfilePic.transferTo(new File(savePath + changeName));
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+		}
+		return changeName;
 	}
 	
 }
