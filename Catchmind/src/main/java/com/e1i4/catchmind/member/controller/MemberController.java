9e2af12913c33524a2c417737e12025a26992132
www78 <subin708@naver.com>
2022-07-13 18:15:00 +0900

    member VO 수정
    member VO Date> String 수정

--- Catchmind/src/main/java/com/e1i4/catchmind/member/controller/MemberController.java
+++ Catchmind/src/main/java/com/e1i4/catchmind/member/controller/MemberController.java
@@ -61,7 +61,7 @@ public class MemberController {
 								Member m,
 								HttpSession session) {		
 		Member loginUser = memberService.loginMember(m);
-
+		
 		if(loginUser == null) {
 			session.setAttribute("alertMsg", "일치하는 회원정보가 없습니다.");
 			
@@ -80,6 +80,7 @@ public class MemberController {
 	// 로그인 시 recentLogin 업데이트(update)
 	public int updateRecentLogin(Member m) {
 		int updateRecentLogin = memberService.updateRecentLogin(m);
+		
 		return updateRecentLogin;
 	}
 	
