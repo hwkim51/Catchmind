229abf4eb7042845441da08fd26d6ca80919c8ee
hi-inbeom <id4mail2g@gmail.com>
2022-07-20 17:13:50 +0900

    매치리스트 완료
    필터 및 팔로우 차단 완료

--- Catchmind/src/main/java/com/e1i4/catchmind/chat/controller/MatchController.java
+++ Catchmind/src/main/java/com/e1i4/catchmind/chat/controller/MatchController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.e1i4.catchmind.chat.model.dao.MatchDao;
+import com.e1i4.catchmind.chat.model.vo.SearchMatch;
 import com.e1i4.catchmind.member.model.vo.Block;
 import com.e1i4.catchmind.member.model.vo.Follow;
 import com.e1i4.catchmind.member.model.vo.Member;
+import com.google.gson.Gson;
 
 @Controller
 public class MatchController {
@@ -44,23 +46,98 @@ public class MatchController {
 	
 	@ResponseBody
 	@RequestMapping(value="block.ma", produces="text/html; charset=UTF-8;")
-	public void insertBlock(int userNo, int block) {
+	public String insertBlock(int userNo, int block) {
 		Block b = new Block();
 		
 		b.setUserNo(userNo); // 차단한 유저
 		b.setBlockedUser(block); // 차단당한 유저
 		
 		int result = mdao.insertBlock(sqlSession, b);
+		
+		return (result>0)? "success" : "fail";
 	}
 	
 	@ResponseBody
 	@RequestMapping(value="follow.ma", produces="text/html; charset=UTF-8;")
-	public void insertFollowed(int userNo, int follow) {
+	public String insertFollowed(int userNo, int follow) {
 		Follow f = new Follow();
 		
 		f.setFoUser(userNo);
 		f.setFoedUser(follow);
 		
 		int result = mdao.insertFollow(sqlSession, f);
+		
+		return (result>0)? "success" : "fail";
+	}
+	
+	@RequestMapping(value="searchList.ma", produces="text/html; charset=UTF-8;")
+	public String searchList(String mbti, String hlow, String hhigh, String alow, String ahigh, HttpSession session, Model model) {
+		SearchMatch m = new SearchMatch();
+		m.setGender(((Member) session.getAttribute("loginUser")).getGender());
+		m.setUserNo(((Member) session.getAttribute("loginUser")).getUserNo());
+		
+		System.out.println(mbti);
+		System.out.println(hlow);
+		System.out.println(hhigh);
+		System.out.println(alow);
+		System.out.println(ahigh);
+		
+		
+		if (mbti.equals("")) {
+			if (!(hlow.equals(""))) {
+				m.setHlow(Integer.parseInt(hlow));
+			} else {
+				m.setHlow(0);
+			}
+			if (!(hhigh.equals(""))) {
+				m.setHhigh(Integer.parseInt(hhigh));
+			} else {
+				m.setHhigh(500);
+			}
+			if (!(alow.equals(""))) {
+				m.setAlow(Integer.parseInt(alow));
+			} else {
+				m.setAlow(0);
+			}
+			if (!(ahigh.equals(""))) {
+				m.setAhigh(Integer.parseInt(ahigh));
+			} else {
+				m.setAhigh(500);
+			}
+			ArrayList<Member> result = mdao.searchList1(sqlSession, m);
+
+			model.addAttribute("mlist", result);
+			
+			return "chat/matchListView2";
+		} else {
+			m.setMbti(mbti);
+			if (!(hlow.equals(""))) {
+				m.setHlow(Integer.parseInt(hlow));
+			} else {
+				m.setHlow(0);
+			}
+			if (!(hhigh.equals(""))) {
+				m.setHhigh(Integer.parseInt(hhigh));
+			} else {
+				m.setHhigh(500);
+			}
+			if (!(alow.equals(""))) {
+				m.setAlow(Integer.parseInt(alow));
+			} else {
+				m.setAlow(0);
+			}
+			if (!(ahigh.equals(""))) {
+				m.setAhigh(Integer.parseInt(ahigh));
+			} else {
+				m.setAhigh(500);
+			}
+			ArrayList<Member> result = mdao.searchList2(sqlSession, m);
+
+			model.addAttribute("mlist", result);
+			
+			return "chat/matchListView2";
+		}
+		
+		
 	}
 }
--- Catchmind/src/main/java/com/e1i4/catchmind/chat/model/dao/MatchDao.java
+++ Catchmind/src/main/java/com/e1i4/catchmind/chat/model/dao/MatchDao.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.e1i4.catchmind.chat.model.vo.SearchMatch;
 import com.e1i4.catchmind.member.model.vo.Block;
 import com.e1i4.catchmind.member.model.vo.Follow;
 import com.e1i4.catchmind.member.model.vo.Member;
@@ -23,6 +24,14 @@ public class MatchDao {
 
 	public int insertFollow(SqlSessionTemplate sqlSession, Follow f) {
 		return sqlSession.insert("matchMapper.insertFollow", f);
+	}
+
+	public ArrayList<Member> searchList1(SqlSessionTemplate sqlSession, SearchMatch m) {
+		return (ArrayList) sqlSession.selectList("matchMapper.searchList1",m);
+	}
+
+	public ArrayList<Member> searchList2(SqlSessionTemplate sqlSession, SearchMatch m) {
+		return (ArrayList) sqlSession.selectList("matchMapper.searchList2",m);
 	}
 
 }
