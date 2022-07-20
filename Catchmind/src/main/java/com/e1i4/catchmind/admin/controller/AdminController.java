b8b452a4ae32d40b621701cf0c0fdaaf7b6ecde4
hwkim51 <108244500+hwkim51@users.noreply.github.com>
2022-07-21 06:24:51 +0900

    오류 발생시 메인페이지 리다이렉트
    HWKim

--- Catchmind/src/main/java/com/e1i4/catchmind/admin/controller/AdminController.java
+++ Catchmind/src/main/java/com/e1i4/catchmind/admin/controller/AdminController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -544,6 +545,12 @@ public class AdminController {
 			return "common/errorPage";
 		}
 	
+	}
+	
+	@GetMapping("/error")
+	public String error(HttpSession session) {
+		session.setAttribute("alertMsg", "잘못된 접근입니다.\n메인 페이지로 이동했습니다.");
+		return "redirect:/";
 	}
 	
 	
